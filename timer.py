a113261c0b20fb92fe0d62b0ee5a0cf8bbdebf4b
Lysuzzz <lysuzzz.xyz@gmail.com>
2024-10-03 21:15:02 +0000

    Full Timer

--- timer.py
+++ timer.py
@@ -1,6 +1,13 @@
 import time
 
-seconds = int(input('Enter seconds: '))
+input_time = int(input('Enter seconds: '))
 
-for x in range(seconds):
-    print(x) 
+for x in reversed(range(input_time)):
+    seconds = x % 60 
+    minutes = int(x/60) % 60
+    hours = int(x/3600) 
+    print(f'{hours:02}:{minutes:02}:{seconds:02}')
+    time.sleep(1)
+
+print('Timer is up')
+
